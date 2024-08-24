d4c47184a372ebf33892fe0e3fd4f46d54d92bf8
SignalMQ <matnazarovsobirjon123@gmail.com>
2024-08-25 01:53:17 +0500

    Upgrade to 4.57

--- e-imzo/opt/E-IMZO/E-IMZO.sh
+++ e-imzo/opt/E-IMZO/E-IMZO.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-java -jar /opt/E-IMZO/E-IMZO.jar
+# check if MacOS
+if [ -d /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin ]; then
+	ln -sf /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java ./java
+	./java -jar E-IMZO.jar
+else
+        cd `dirname $0`
+	java -jar E-IMZO.jar
+fi
 
 exit 0
 
